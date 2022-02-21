154c0ee5c2f7bb8e9d68b4a811d3ce3dc0e28af8
김다은 <kimdaeun@indentcorp.com>
2022-02-21 18:06:23 +0900

    ✨ Add mouse follow

--- src/home/room/index.tsx
+++ src/home/room/index.tsx
@@ -1,12 +1,41 @@
 import { Link } from "react-router-dom";
+import useMouse from "@react-hook/mouse-position";
+import { useEffect, useRef, useState } from "react";
+import styled from "styled-components";
 
 const Room = () => {
+  const [xPosition, setXPosition] = useState<number>(0);
+  const [yPosition, setYPosition] = useState<number>(0);
+  const ref = useRef(null);
+  const mouse = useMouse(ref, { enterDelay: 0, leaveDelay: 0 });
+
+  useEffect(() => {
+    if (mouse.x) setXPosition(mouse.x + 20);
+    if (mouse.y) setYPosition(mouse.y + 20);
+  }, [mouse.x, mouse.y]);
   return (
-    <div>
+    <Container ref={ref}>
       <h1>room</h1>
+      {mouse.x && mouse.y && (
+        <Test x={xPosition} y={yPosition}>
+          is follow?
+        </Test>
+      )}
       <Link to="/dodan9.io/ocean">to ocean</Link>
-    </div>
+    </Container>
   );
 };
 
 export default Room;
+
+const Container = styled.div`
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+`;
+const Test = styled.div<{ x: any; y: any }>`
+  position: fixed;
+  top: ${(props) => props.y}px;
+  left: ${(props) => props.x}px;
+  transition: 0.1s ease-in-out;
+`;
