26df44f88af582daa60604b56d7ca90586c7ef69
DODAN9 <9139a90@naver.com>
2022-08-14 17:07:18 +0900

    add converter
    practice input & modifier

--- src/home/todo/index.tsx
+++ src/home/todo/index.tsx
@@ -1,16 +1,61 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import { createGlobalStyle } from "styled-components";
 
 const Todo = () => {
   const [count, setCount] = useState<number>(0);
   const upCount = () => {
     setCount((count) => count + 1);
   };
+  const downCount = () => {
+    setCount((count) => count - 1);
+  };
+
+  const [minutes, setMinutes] = useState<number | "">("");
+  const onMinuteChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value)
+      setMinutes((minutes) => (minutes = parseFloat(e.target.value)));
+    else setMinutes("");
+  };
+
   return (
     <div>
-      <button onClick={upCount}>click</button>
-      <span>{count}</span>
+      <GlobalStyle />
+
+      <section>
+        <button onClick={upCount}>count up</button>
+        <button onClick={downCount}>count down</button>
+        <span>{count}</span>
+      </section>
+
+      <section>
+        <h3>Converter</h3>
+        <label htmlFor="minutes">Minutes</label>
+        <input
+          id="minutes"
+          placeholder="minutes"
+          type="number"
+          value={minutes}
+          onChange={onMinuteChange}
+        />
+        <br />
+        <label htmlFor="hours">Hours</label>
+        <input
+          id="hours"
+          placeholder="hours"
+          type="number"
+          value={minutes ? minutes / 60 : ""}
+          readOnly
+        />
+      </section>
     </div>
   );
 };
 
 export default Todo;
+
+const GlobalStyle = createGlobalStyle`
+box-sizing: border-box;
+section{
+  padding: 40px;
+}
+`;
