0da1c835353e24a46a325be50b7d05c79a1051d0
DODAN9 <9139a90@naver.com>
2022-09-11 02:29:17 +0900

    add walk to meet pokemon one by one
    - api를 활용하여 해당 지역에서 등장하는 포켓몬을 walk 버튼을 누를 때마다 랜덤으로 만날 수 있다.

--- src/home/pokemon2/MeetModal.tsx
+++ src/home/pokemon2/MeetModal.tsx
@@ -1,15 +1,111 @@
-import { Dispatch, SetStateAction } from "react";
+import axios from "axios";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import styled from "styled-components";
 
 interface propsType {
   url: string;
   closeFunction: Dispatch<SetStateAction<boolean>>;
 }
+interface encounterDetailType {
+  chance: number;
+  max_level: number;
+  min_level: number;
+}
+interface pokemonEncounterType {
+  pokemon: {
+    name: string;
+    url: string;
+  };
+  version_details: [
+    {
+      encounter_details: encounterDetailType[];
+    }
+  ];
+}
+interface pokemonEncounterListType {
+  name: string;
+  pokemon_encounters: pokemonEncounterType[];
+}
+interface pokemonDataType {
+  shiny: boolean;
+  run: boolean;
+  catch: boolean;
+  id: number;
+  name: string;
+  sprites: { front_default: string; front_shiny: string };
+  chance: number;
+  level: number;
+}
 
 const MeetModal = ({ url, closeFunction }: propsType) => {
-  console.log("open");
+  const [pokemonEncounterList, setPokemonEncounterList] =
+    useState<pokemonEncounterListType>();
+  const [pokemonData, setPokemonData] = useState<pokemonDataType>();
+
+  const getRandomNumber = (max: number, min: number) => {
+    if (max == min) return 0;
+    const randomNumber = Math.floor(Math.random() * max - min + 1) + min;
+    return randomNumber;
+  };
+
+  const getLocationAreaApi = async () => {
+    const response = await axios({
+      url: `${url}`,
+      method: "get",
+    });
+    setPokemonEncounterList(response.data);
+  };
+
+  const getRandomPokemon = async () => {
+    if (pokemonEncounterList) {
+      const pokemonList = pokemonEncounterList.pokemon_encounters;
+      const randomPkm = pokemonList[getRandomNumber(pokemonList.length - 1, 0)];
+      const randomPkmDetail =
+        randomPkm.version_details[
+          getRandomNumber(randomPkm.version_details.length - 1, 0)
+        ];
+      const randomPkmEncounterDetail = randomPkmDetail.encounter_details[0];
+      const response = await axios({
+        url: randomPkm.pokemon.url,
+        method: "get",
+      });
+      response.data.shiny = getRandomNumber(100, 1) > 95 ? true : false;
+      response.data.run = false;
+      response.data.catch = false;
+      response.data.chance = randomPkmEncounterDetail.chance;
+      response.data.level = getRandomNumber(
+        randomPkmEncounterDetail.max_level,
+        randomPkmEncounterDetail.min_level
+      );
+      setPokemonData(response.data);
+    }
+  };
+
+  useEffect(() => {
+    getLocationAreaApi();
+  }, []);
+
   return (
     <>
-      <div>Modal</div>
+      <div>{pokemonEncounterList?.name}</div>
+      <button onClick={getRandomPokemon}>walk</button>
+      {pokemonData ? (
+        <Pokemon>
+          <Name shiny={pokemonData.shiny}>{pokemonData.name}</Name>
+
+          <img
+            src={
+              pokemonData.shiny
+                ? pokemonData.sprites.front_shiny
+                : pokemonData.sprites.front_default
+            }
+          />
+          <Detail>
+            <span>{pokemonData.chance}%</span>
+            <button>catch!</button>
+          </Detail>
+        </Pokemon>
+      ) : null}
       <button
         onClick={() => {
           closeFunction(false);
@@ -22,3 +118,39 @@ const MeetModal = ({ url, closeFunction }: propsType) => {
 };
 
 export default MeetModal;
+
+const Pokemon = styled.div`
+  position: relative;
+  margin: 10px;
+  padding: 14px;
+  width: 200px;
+  height: 140px;
+  background-color: #b1c5f1;
+  text-align: center;
+  border-radius: 5px;
+  @media screen and (max-width: 500px) {
+    width: 120px;
+    height: 100px;
+    margin: 5px;
+    padding: 8px;
+    & img {
+      width: 70px;
+    }
+  }
+  @media screen and (min-width: 500px) and (max-width: 800px) {
+    width: 160px;
+  }
+`;
+
+const Name = styled.div<{ shiny: boolean }>`
+  color: ${(props) => (props.shiny ? "red" : "black")};
+`;
+
+const Detail = styled.span`
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  & button {
+    margin-left: 5px;
+  }
+`;
