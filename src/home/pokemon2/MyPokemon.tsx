c111ee3e157edfbad45058bf92cd9d6181140a15
DODAN9 <9139a90@naver.com>
2022-09-14 20:17:28 +0900

    create my pokemon component

---
+++ src/home/pokemon2/MyPokemon.tsx
@@ -0,0 +1,9 @@
+const MyPokemon = () => {
+  return (
+    <div>
+      <h2>My Pokemon Box</h2>
+    </div>
+  );
+};
+
+export default MyPokemon;
--- src/home/pokemon2/index.tsx
+++ src/home/pokemon2/index.tsx
@@ -2,27 +2,53 @@ import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Region from "./Region";
 import Location from "./Location";
 import Country from "./Counrty";
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
+import MyPokemon from "./MyPokemon";
 
 const Pokemon2 = () => {
   return (
-    <div>
+    <Container>
+      <GlobalStyle />
       <Header>
-        <div>Pokemon2</div>
-        <div>My Pokemon</div>
+        <h2>
+          <Link to='/dodan9.io/pokemon2/'>Pokemon2</Link>
+        </h2>
+        <h2>
+          <Link to='/dodan9.io/pokemon2/mypokemon'>My Pokemon</Link>
+        </h2>
       </Header>
       <Routes>
+        <Route path='/mypokemon' element={<MyPokemon />} />
         <Route path='/' element={<Country />} />
         <Route path='/:region' element={<Region />} />
         <Route path='/:region/:location' element={<Location />} />
       </Routes>
-    </div>
+    </Container>
   );
 };
 
 export default Pokemon2;
 
+const GlobalStyle = createGlobalStyle`
+a{
+  color: black;
+}
+  a:visited{
+    color:black;
+  }
+  a:hover{
+    text-decoration: underline;
+    color: blue;
+  }
+`;
+const Container = styled.div`
+  margin: 10px;
+`;
 const Header = styled.div`
   width: 100%;
   display: flex;
+  justify-content: space-between;
+  & h2 {
+    margin: 0;
+  }
 `;
